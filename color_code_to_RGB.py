908de022dd174fc78a83e5725dc683a075d5a4a7
teppei <teppei0830.4869@gmail.com>
2023-07-12 22:24:47 +0900

    新たに color_code_to_RGB.py を作成しました。
    これはカラーコードを受け取って、 RGBColor オブジェクトに直すモジュールです。

---
+++ color_code_to_RGB.py
@@ -0,0 +1,9 @@
+from pptx.dml.color import RGBColor
+    
+def color_code_to_RGB_func(color):
+
+    #
+    R = int(color[1:3], 16)
+    G = int(color[3:5], 16)
+    B = int(color[5:7], 16)
+    return RGBColor(R, G, B)
