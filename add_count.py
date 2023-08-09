4a23b6d2d131ba34c8db6ac283a1b1be50525e5d
teppei <teppei0830.4869@gmail.com>
2023-08-09 20:26:11 +0900

    add_count.py, add_date.py, main.py を修正しました。
    add_count.py, add_count.py の関数の引数に文字色を指定するための
    変数 text_RGB を追加しました。
    これに伴って、 main.py から関数を呼ぶときの引数を増やしました。

--- add_count.py
+++ add_count.py
@@ -3,8 +3,8 @@ from pptx.dml.color import RGBColor
 
 from add_box import add_box_func 
 
-def add_count_func(sld, count):
+def add_count_func(sld, count, text_RGB):
 
-    sld = add_box_func(sld, fontsize=Pt(300), text=count, text_RGB=RGBColor(255, 255, 255), lm=Pt(100), tm=Pt(250), h=Pt(50), w=Pt(500) )
-    sld = add_box_func(sld, fontsize=Pt(50), text="卒業まであと", text_RGB=RGBColor(255, 255, 255), lm=Pt(10), tm=Pt(100), h=Pt(50), w=Pt(300) )
-    sld = add_box_func(sld, fontsize=Pt(100), text="日", text_RGB=RGBColor(255, 255, 255), lm=Pt(380), tm=Pt(310), h=Pt(50), w=Pt(500) )
+    sld = add_box_func(sld, fontsize=Pt(300), text=count, text_RGB=text_RGB, lm=Pt(100), tm=Pt(250), h=Pt(50), w=Pt(500) )
+    sld = add_box_func(sld, fontsize=Pt(50), text="卒業まであと", text_RGB=text_RGB, lm=Pt(10), tm=Pt(100), h=Pt(50), w=Pt(300) )
+    sld = add_box_func(sld, fontsize=Pt(100), text="日", text_RGB=text_RGB, lm=Pt(380), tm=Pt(310), h=Pt(50), w=Pt(500) )
