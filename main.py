bdac545106335b2116f3235e372c00401cfe4313
teppei <teppei0830.4869@gmail.com>
2023-08-09 20:54:02 +0900

    main.py と color_code_to_RGB.py を修正しました。
    color_code_to_RGB_func の返り値を dict 型にしました。
    これは値の変更をしやすくするためです。

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from color_code_to_RGB import color_code_to_RGB_func
 date_st = '2023-08-09'
 date_ed = '2024-03-31'
 bg_color = '#00ff00'
-text_color = '#ffffff'
+text_color = '#00ffff'
 
 prs = pptx.Presentation()
 delta_days = calc_delta_days_func(date_st, date_ed)
@@ -35,13 +35,13 @@ for i in range(delta_days, -1, -1):
     # 背景色を設定
     # (直接背景色の設定は難しそうなので大きめの色付きテキストボックスを配置
     #　そのせいで少しスライドが大きく見える。スライドショーにすれば文字は真ん中に来る。)
-    add_box_func(last_sld, bg_RGB=bg_rgb)
+    add_box_func(last_sld, bg_RGB=RGBColor(bg_rgb['r'], bg_rgb['g'], bg_rgb['b']))
 
     # 最後のスライドに残り日数を追加
-    add_count_func(last_sld, str(i), text_RGB=text_rgb)
+    add_count_func(last_sld, str(i), text_RGB=RGBColor(text_rgb['r'], text_rgb['g'], text_rgb['b']))
     
     # 最後のスライドに日付を追加
-    add_date_func(last_sld, date_now, text_RGB=text_rgb)
+    add_date_func(last_sld, date_now, text_RGB=RGBColor(text_rgb['r'], text_rgb['g'], text_rgb['b']))
 
     # 日付を更新
     date_now = add_days_func(date_now) 
